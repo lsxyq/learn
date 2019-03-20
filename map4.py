b59d65ab24b679f741825ff657bf885576d75915
xyq <454922491@qq.com>
2019-03-20 18:15:49 +0800

    高颜值图片爬取

--- map4.py
+++ map4.py
@@ -1,80 +1,30 @@
-#!/usr/bin/env python
-# -*- coding:UTF-8 -*-
-# Author:Leslie-x
-import wave
-from pyaudio import PyAudio,paInt16
-from PIL import ImageGrab
-import numpy as np
-import cv2
-from moviepy.editor import *
-from moviepy.audio.fx import all
 import time
+import os
+import re
+import pydash
+import base64
+from aip import AipFace
 
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 2
-RATE = 44100
-WAVE_OUTPUT_FILENAME = "output.wav"
+APP_ID = "15793380"
+API_KEY = "aClspWsLfxELstGxnFYEPSjq"
+SECRET_KEY = "oLKUE8liCG7A1LnrEjyGyR2XRg3prMHL"
 
-p = pyaudio.PyAudio()
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-audio_record_flag = True
-def callback(in_data, frame_count, time_info, status):
-    wf.writeframes(in_data)
-    if audio_record_flag:
-        return (in_data, pyaudio.paContinue)
-    else:
-        return (in_data, pyaudio.paComplete)
-stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                channels=wf.getnchannels(),
-                rate=wf.getframerate(),
-                input=True,
-                stream_callback=callback)
-image = ImageGrab.grab()#获得当前屏幕
-width = image.size[0]
-height = image.size[1]
-print("width:", width, "height:", height)
-print("image mode:",image.mode)
-k=np.zeros((width,height),np.uint8)
+options = {"face_field": "age,gender,beauty,qualities"}
 
-fourcc = cv2.VideoWriter_fourcc(*'XVID')#编码格式
-video = cv2.VideoWriter('test.mp4', fourcc, 9.5, (width, height))
-#经实际测试，单线程下最高帧率为10帧/秒，且会变动，因此选择9.5帧/秒
-#若设置帧率与实际帧率不一致，会导致视频时间与音频时间不一致
 
-print("video recording!!!!!")
-stream.start_stream()
-print("audio recording!!!!!")
-record_count = 0
-while True:
-    img_rgb = ImageGrab.grab()
-    img_bgr=cv2.cvtColor(np.array(img_rgb), cv2.COLOR_RGB2BGR)#转为opencv的BGR格式
-    video.write(img_bgr)
-    record_count += 1
-    if(record_count > 200):
-        break
-    print(record_count, time.time())
+def init_client(app_id, api_key, secret_key):
+    client = AipFace(app_id, api_key, secret_key)
+    return client
 
-audio_record_flag = False
-while stream.is_active():
-    time.sleep(1)
 
-stream.stop_stream()
-stream.close()
-wf.close()
-p.terminate()
-print("audio recording done!!!!!")
+def detective(image, options):
+    client = init_client(APP_ID, API_KEY, SECRET_KEY)
+    image = str(base64.b64encode(image), "utf-8")
+    response = client.detect(str(image), "BASE64", options)
+    response = response.get("result")
+    print(response)
 
-video.release()
-cv2.destroyAllWindows()
-print("video recording done!!!!!")
 
-print("video audio merge!!!!!")
-audioclip = AudioFileClip("output.wav")
-videoclip = VideoFileClip("test.mp4")
-videoclip2 = videoclip.set_audio(audioclip)
-video = CompositeVideoClip([videoclip2])
-video.write_videofile("test2.mp4",codec='mpeg4')
+image = open("D:\learn\image\80--然新--你见过哪些惊为天人的人间绝色？要区别于一般美女或帅哥的？--0.jpg", "rb").read()
+
+detective(image, options)
--- 录影.py
+++ 录影.py
@@ -2,25 +2,30 @@
 # -*- coding:UTF-8 -*-
 # Author:Leslie-x
 import subprocess
+from subprocess import Popen, PIPE
+import subprocess
+import os
 
-ffmpegPath = r"D:\learn\ffmpeg\bin\ffmpeg.exe"
-
-d = r'D:\learn\ffmpeg\bin\ffmpeg.exe -f gdigrab -i desktop -f dshow -i audio="virtual-audio-capturer" -vcodec libx264 -preset:v ultrafast -tune:v zerolatency -r 25 -acodec libmp3lame '
+d = r'ffmpeg.exe -f gdigrab -offset_x 0 -offset_y 0 -video_size 1366x768 -i desktop -f dshow -i audio="virtual-audio-capturer" -vcodec libx264 -preset:v ultrafast -tune:v zerolatency -r 24 -y -acodec aac -ac 1 -ar 44100 -ab 64k d:\out.mp4'
 
 
-class CutSplicingVdeio(object):
+class CutSplicingVideo(object):
     def __init__(self):
+        self.p = None
         pass
 
-    def instructions(self):
-        dercription = "vdeio and image transform,vdeio other opreation"
-        return dercription
-
-    def transcribeScreen(self, filePath):
-        cmd = d+filePath
-        print(cmd)
-        subprocess.call(cmd)
+    def transcribeScreen(self):
+        os.system(d)
 
 
-vp = CutSplicingVdeio()
-vp.transcribeScreen(r"D:\learn\record.mp4")
+    def kill_cmd(self):
+        self.p.terminate()
+
+
+vp = CutSplicingVideo()
+vp.transcribeScreen()
+
+
+
+
+
